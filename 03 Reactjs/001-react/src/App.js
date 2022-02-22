b0a3e21aa055846634983229b22212d4ae5fb6a7
rishabshalla <rishabshalla@gmail.com>
2022-02-22 20:32:33 +0530

    first

---
+++ 03 Reactjs/001-react/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Hello } from "./components/Hello";
+import style from "./components/style.module.css";
+
+function App() {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ 03 Reactjs/001-react/src/components/Functional.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Functional() {
+  return <div>Functional</div>;
+}
+
+export default Functional;
